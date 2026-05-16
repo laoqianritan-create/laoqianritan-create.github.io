8468ac845bc323dc48e50e5d86777725e0fbaa32
laoqianritan-create <noreply@github.com>
2026-05-16 13:48:08 +0800

    update us-debt dashboard 2026-05-16 13:47

---
+++ us-debt/_next/static/FHeWiCBpWRFX1tV-zE3-s/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/us-debt//_next/:path+",
+        "destination": "/us-debt/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
